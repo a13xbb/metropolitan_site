d7999e5e9d10107c74fd09bb9a6039668baa3ae5
a13xbb <alex.blokh24@gmail.com>
2023-02-19 01:22:32 +0300

    feat: form wip

--- src/components/LegalServices/index.jsx
+++ src/components/LegalServices/index.jsx
@@ -1,10 +1,13 @@
+import { RequestForm } from "./Form";
+
 import styles from "./LegalServices.module.scss";
 
 export const LegalServices = (props) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.form_wrapper}></div>
-      <div className={styles.info_wrapper}></div>
+      <div className={styles.form_wrapper}>
+        <RequestForm />
+      </div>
     </div>
   );
 };
