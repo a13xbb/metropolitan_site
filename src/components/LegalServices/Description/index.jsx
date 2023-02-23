09ed942b0613e45abb6b3140945b39a5e0dcd70d
a13xbb <alex.blokh24@gmail.com>
2023-02-23 03:25:20 +0300

    form description done

---
+++ src/components/LegalServices/Description/index.jsx
@@ -0,0 +1,20 @@
+import styles from "./Description.module.scss";
+
+export const Description = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.blue_block}>
+        <h1>Юридические услуги в ОАЭ</h1>
+        <div className={styles.white_line}></div>
+      </div>
+      <div className={styles.description}>
+        <h3>Metropolitan Consulting работает на рынке ОАЭ более 10 лет.</h3>
+        <p>
+          За это время компания зарекомендовала себя как надежный партнер по
+          урегулированию юридических и административных вопросов наших клиентов
+          в ОАЭ.
+        </p>
+      </div>
+    </div>
+  );
+};
