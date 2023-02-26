88eb6f4ec2b9e5a7bb8c311f53187344aabc4ec9
a13xbb <alex.blokh24@gmail.com>
2023-02-26 22:31:56 +0300

    metropolitan consulting block started

---
+++ src/components/MetropolitanConsulting/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./MetropolitanConsulting.module.scss";
+
+export const MetropolitanConsulting = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <div className={styles.blue_line}></div>
+        <h1>Metropolitan Consulting</h1>
+        <div className={styles.blue_line}></div>
+      </div>
+    </div>
+  );
+};
